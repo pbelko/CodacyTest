4608cab3c76bc2da6d5c9b89ff0dc4aba4eed629
Patrick Belko <pbelko@enternest.com>
2019-10-14 15:59:23 -0400

    Intentional error

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -8,21 +8,23 @@ import * as app from "tns-core-modules/application";
 })
 export class SettingsComponent implements OnInit {
 
+
     constructor() {
         // Use the component constructor to inject providers.
-    }
+    };
 
     ngOnInit(): void {
         // Init your component properties here.
-    }
+    };
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
 
         if(1 == 1){
-            console.log('some thing');
+
         };
 
+        return;
         sideDrawer.showDrawer();
     }
 }
